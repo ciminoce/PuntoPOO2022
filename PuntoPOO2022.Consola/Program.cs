d53f05c5a14546e57199098a81a8735db6d78137
ciminoce <ciminoce@yahoo.com.ar>
2022-05-03 09:29:10 -0300

    Agregar archivos de proyecto.

---
+++ PuntoPOO2022.Consola/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MisNuevasFunciones;
+using PuntoPOO2022.Entidades;
+
+namespace PuntoPOO2022.Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var coordX = IngresoDatos.PedirInt("Ingrese Coord. X del punto:");
+            var coordY = IngresoDatos.PedirInt("Ingrese Coord. Y del punto:");
+
+            Punto punto=new Punto(coordX,coordY);
+
+            Console.WriteLine($"Coord. Cartesianas={punto.ToString()}");
+            Console.WriteLine($"Coord. Polares={punto.GetCoordPolares()}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ PuntoPOO2022.Entidades/Punto.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace PuntoPOO2022.Entidades
+{
+    public class Punto
+    {
+        
+        public int CoordX { get; set; }
+        public int CoordY { get; set; }
+
+        public Punto()
+        {
+            
+        }
+        public Punto(int valorX, int valorY)
+        {
+            this.CoordX = valorX;
+            this.CoordY = valorY;
+        }
+
+        public double GetDistanciaAlOrigen() => Math.Sqrt(Math.Pow(this.CoordX, 2) + Math.Pow(this.CoordY, 2));
+
+        public Cuadrante GetCuadrante()
+        {
+            if (this.CoordX>=0 && this.CoordY>=0)
+            {
+                return Cuadrante.Primero;
+            }else if (this.CoordX<0 && this.CoordY>0)
+            {
+                return Cuadrante.Segundo;
+            }else if (this.CoordX < 0 && this.CoordY < 0)
+            {
+                return Cuadrante.Tercero;
+            }
+            else
+            {
+                return Cuadrante.Cuarto;
+            }
+        }
+
+        public string GetCoordPolares()
+        {
+            var r = this.GetDistanciaAlOrigen();
+            var phi = Math.Atan(this.CoordY / this.CoordX);
+            return $"({r};{phi})";
+        }
+
+        public override string ToString()
+        {
+            return $"({this.CoordX},{this.CoordY})";
+        }
+    }
+}
